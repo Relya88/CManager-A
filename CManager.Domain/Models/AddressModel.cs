cc1ba68a73dea83d230fcda3f011973929600f8d
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2025-12-16 17:52:00 +0100

    Added Domain project with CustomerModel and AddressModel

---
+++ CManager.Domain/Models/AddressModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Domain.Models;
+
+//klassen som representerar en adress
+public class AddressModel
+{
+    public string StreetAddress { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+}
---
+++ CManager.Domain/Models/CustomerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Domain.Models;
+
+//Klass som inninnehåller den information som behövs för att hantera kund
+//med allt från unikt id för varje kund som skapats med guid till komplett adressobjekt tillhörande kund
+
+public class CustomerModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public AddressModel Address { get; set; } = new AddressModel();
+}
+
